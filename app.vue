c99e831f2c8467bb6cafcfc945894b2e41badb30
AdrianW <adrianwijaya584@gmail.com>
2023-05-25 20:47:03 +0800

    add some meta

--- app.vue
+++ app.vue
@@ -12,15 +12,6 @@
   import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
   useSeoMeta({
-    description: 'web keren pokoknya',
-    ogImage: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-leafgreen-version/e/e2/Pokemon-leafgreen-box.jpg',
-    twitterCard: 'summary_large_image',
-    twitterSite: '@adrianwijaya584',
-    twitterCreator: '@adrianwijaya584',
-    twitterTitle: 'Web test',
-    twitterDescription: 'Hello ini web test ya',
-    twitterImage: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-leafgreen-version/e/e2/Pokemon-leafgreen-box.jpg',
-    appleMobileWebAppTitle: 'test',
+    description: 'test123'
   })
-
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,6 @@ export default defineNuxtConfig({
     shim: false
   },
   ssr: false,
-  app: {
-    head: {
-      title: 'Nuxt 3.5 preview test'
-    }
-  },
   modules: [
     '@pinia/nuxt',
     '@nuxt/image-edge',
@@ -23,5 +18,52 @@ export default defineNuxtConfig({
   },
   tailwindcss: {
     configPath: 'tailwind.config.js'
-  }
+  },
+  app: {
+    head: {
+      title: 'Nuxt 3.5 preview test',
+      meta: [
+        {
+          name: 'description',
+          content: 'web keren pokoknya'
+        },
+        {
+          name: 'og:title',
+          content: 'web cool'
+        },
+        {
+          name: 'og:image',
+          content: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-leafgreen-version/e/e2/Pokemon-leafgreen-box.jpg',
+        }, 
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image'
+        },
+        {
+          name: 'twitter:site',
+          content: '@adrianwijaya584' 
+        },
+        {
+          name: 'twitter:site',
+          content: '@adrianwijaya584' 
+        },
+        {
+          name: 'twitter:creator',
+          content: '@adrianwijaya584' 
+        },
+        {
+          name: 'twitter:title',
+          content: 'web test' 
+        },
+        {
+          name: 'twitter:description',
+          content: 'web keren pokoknya' 
+        },
+        {
+          name: 'twitter:image',
+          content: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-leafgreen-version/e/e2/Pokemon-leafgreen-box.jpg' 
+        }
+      ],
+    }
+  },
 })
