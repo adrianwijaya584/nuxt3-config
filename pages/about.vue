6ecefe03bdb06a6fec00da17fa82e4e2b4c19be0
AdrianW <adrianwijaya584@gmail.com>
2023-07-04 13:11:14 +0800

    remove image from about

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="">
     <h3 class="text-center font-bold text-3xl">About us</h3>
-    <NuxtImg
-      preload
-      loading="lazy"
-      src="https://i.pinimg.com/736x/1b/88/92/1b8892d1ee65e258a2ce7804f52c5f9a.jpg"
-      height="200" 
-      width="200" 
-      alt="ryo yamada"
-      class="self-start"
-    />
 
       <NuxtImg
         src="/jeanne.jpg"
